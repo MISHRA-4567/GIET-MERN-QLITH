f85d436d9c1431040e1e4c041cbc1ef1c428e415
MISHRA-4567 <mishranandita799@gmail.com>
2026-01-08 14:19:46 +0530

    Add files via upload

---
+++ day - 18/movies.js
@@ -0,0 +1,12 @@
+let moviesContainer = document.getElementById("movies-container")
+console.log(moviesContainer)
+
+function displayMovies(movies) {
+    moviesContainer.innerHTML="";
+
+    movies.map(()=>{
+        moviesContainer.innerHTML +='
+
+        '
+    })
+}
