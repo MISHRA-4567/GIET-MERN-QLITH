c8a97c7895875634a77b2233f1b445a3b77b5019
MISHRA-4567 <mishranandita799@gmail.com>
2026-01-16 08:51:04 +0530

    Add files via upload

--- day - 18/movies.js
+++ day - 18/movies.js
@@ -1,12 +1,104 @@
 let moviesContainer = document.getElementById("movies-container")
-console.log(moviesContainer)
+let API_KEY="9a63d197"
+let searchInput=document.getElementById("search-input");
+let searchh8tn=document.getElementById("search-btn");
+let movieModal=document.getElementById("movie-modal")
+let movieContent=document.getElementById("movie-center")
+
+console.log(searchInput)
+console.log(searchh8tn)
+
 
 function displayMovies(movies) {
     moviesContainer.innerHTML="";
 
-    movies.map(()=>{
-        moviesContainer.innerHTML +='
-
-        '
+    movies.map((movie)=>{
+        moviesContainer.innerHTML +=`
+        <div class="rounded bg-white shadow hover:scale-105 transition cursor-pointer">
+        onclick="getMovieDetails('${movie.imdbID}')">
+            <img  
+              src="${movie.Poster}"
+              class="rounded h-64 w-full object-cover"
+            />
+             <div class="p-3">
+               <h3 class="font-bold text-sm">${movie.Title}</h3>
+               <p class="text-xs text-gray-500">${movie.Year}</p>
+              </div>
+        </div>
+        `;
     })
 }
+
+async function loadDefaultMovies() {
+    moviesContainer.innerHTML="<p>Loading ...</p>"
+
+    try {
+        let res=await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=all`); 
+        let data=await res.json();
+        if(data.Response==="False"){
+            moviesContainer.innerHTML="<p class='text-red-500'>No movies found ...</p>"
+
+        }
+        displayMovies(data.Search)
+        
+    } catch (error) {
+        console.log("something went wrong")
+    }
+}
+
+window.addEventListener("load",()=> {
+    loadDefaultMovies()
+})
+
+async function moviesBasedOnSearch() {
+    let search=searchInput.value.trim()
+    console.log(search)
+
+    moviesContainer.innerHTML=""
+
+    try {
+        let res=await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
+        let data=await res.json();
+        if(data.Response==="False"){
+            moviesContainer.innerHTML="<p class='text-red-500'>No movies found ...</p>"
+        }
+        displayMovies(data.Search)
+        console.log(data)
+
+    } catch (error) {
+        console.log("something went wrong")
+    }
+}
+
+searchh8tn.addEventListener("click",()=>{
+    moviesBasedOnSearch()
+})
+
+async function getMoviesDetails(id){
+    try {
+        let res=await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`);
+        let data=await res.json()
+        movieContent.innerHTML= `
+        <img src="${data.Poster}"alt="" class="w-full h-64 object-cever rounded mb-4">
+        <h2 class="text-xl font-bold mb-2">${data.Title}</h2>
+        <p class="text-sm mb-2">${data.Year}</p>
+        <p class="text-sm mb-2">${data.Genre}</p>
+        <p class="text-sm mb-2">${data.Rating}</p>
+        <p class="text-sm text-gray-600">${data.Plot}</p>
+        `
+
+        movieModal.classList.remove("hidden");
+        movieModal.classList.add("flex");
+
+
+    } catch (error) {
+        console.log("somethong went wrong")
+    }
+}
+
+
+function closeModal(){
+     movieModal.classList.add("hidden");
+     movieModal.classList.remove("flex");
+}
+
